ff870b2dcb27f72b12098d27ac9ae4c86b6152e0
Gan Jun Kai <kuhn96@gmail.com>
2017-03-26 00:50:00 +0800

    Add Start, Pause, Reset functions to timer

--- app/app.js
+++ app/app.js
@@ -7,28 +7,94 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      currentMinuteCount: 10,
+      currentSecondCount: 60,
+      intervalId: '',
+    }
+    this.timer = this.timer.bind(this)
+    this.nextMinute = this.nextMinute.bind(this)
     this.onStart = this.onStart.bind(this)
     this.onReset = this.onReset.bind(this)
+    this.onPause = this.onPause.bind(this)
   }
 
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  componentWillUnmount() {
+    // use intervalId from the state to clear the interval
+    clearInterval(this.state.intervalId)
+    this.setState({intervalId: ''})
+    console.log('componentWillUnmount')
+  }
+
+  timer() {
+    // setState method is used to update the state
+    let newSecondCount = this.state.currentSecondCount - 1
+    const newMinuteCount = this.state.currentMinuteCount - 1
+    if(newSecondCount >= 0) {
+      if(this.nextMinute(newSecondCount)) {
+        newSecondCount = 60
+        this.setState(
+          {
+            currentSecondCount: newSecondCount,
+            currentMinuteCount: newMinuteCount
+          }
+        )
+      } else {
+        this.setState({ currentSecondCount: newSecondCount })
+      }
+    } else {
+      clearInterval(this.state.intervalId)
+    }
+  }
+
+  nextMinute(currentSecond) {
+    return currentSecond === 0
+  }
+
+  // shouldComponentUpdate() {
+  //   return this.state.counting
+  // }
+
   onStart() {
-    console.log('onStart')
+    if(this.state.intervalId === '') {
+      const intervalId = setInterval(this.timer, 1000)
+      // store intervalId in the state so it can be accessed later:
+      this.setState({intervalId})
+    } else {
+      console.log("It's running!!!")
+    }
   }
 
   onReset() {
-    console.log('onReset')
+    this.setState(
+      {
+        currentMinuteCount: 10,
+        currentSecondCount: 60
+      }
+    )
+    this.componentWillUnmount()
+  }
+
+  onPause() {
+    this.componentWillUnmount()
   }
 
   // render method is most important
   // render method returns JSX template
   render() {
-      return (
-        <div>
-          <Timer />
-          <Button onClick={this.onStart}>Start</Button>
-          <Button onClick={this.onReset}>Reset</Button>
-        </div>
-      );
+    const currentCount = `${this.state.currentMinuteCount} : ${this.state.currentSecondCount}`
+    return (
+      <div>
+        <Timer currentCount={currentCount}/>
+        <Button onClick={this.onStart}>Start</Button>
+        <Button onClick={this.onPause}>Pause</Button>
+        <Button onClick={this.onReset}>Reset</Button>
+      </div>
+    );
   }
 }
 
--- app/components/Timer/Timer.js
+++ app/components/Timer/Timer.js
@@ -3,55 +3,11 @@ import React from 'react'
 class Timer extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      currentMinuteCount: 10,
-      currentSecondCount: 60,
-      intervalId: ''
-    }
-    this.timer = this.timer.bind(this)
-    this.nextMinute = this.nextMinute.bind(this)
-  }
-
-  componentDidMount() {
-    const intervalId = setInterval(this.timer, 1000)
-    // store intervalId in the state so it can be accessed later:
-    this.setState({intervalId})
-  }
-
-  componentWillUnmount() {
-    // use intervalId from the state to clear the interval
-    clearInterval(this.state.intervalId)
-  }
-
-  timer() {
-    // setState method is used to update the state
-    let newSecondCount = this.state.currentSecondCount - 1
-    const newMinuteCount = this.state.currentMinuteCount - 1
-    if(newSecondCount >= 0) {
-      if(this.nextMinute(newSecondCount)) {
-        newSecondCount = 60
-        this.setState(
-          {
-            currentSecondCount: newSecondCount,
-            currentMinuteCount: newMinuteCount
-          }
-        )
-      } else {
-        this.setState({ currentSecondCount: newSecondCount })
-      }
-    } else {
-      clearInterval(this.state.intervalId)
-    }
-  }
-
-  nextMinute(currentSecond) {
-    return currentSecond === 0
   }
 
   render() {
     // Return JSX via render()
-    const currentCount = `${this.state.currentMinuteCount} : ${this.state.currentSecondCount}`
-
+    const { currentCount } = this.props
     return (
       <div>
         <h1>{currentCount}</h1>
