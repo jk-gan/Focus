a8d17de76aedbb60c7815092d1cc607d4832a629
Gan Jun Kai <kuhn96@gmail.com>
2017-03-25 16:09:22 +0800

    Add count down feature in timer

--- app/components/Timer/Timer.js
+++ app/components/Timer/Timer.js
@@ -4,17 +4,38 @@ class Timer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      minutes: 5,
-      seconds: 60
+      currentCount: 10,
+      intervalId: ''
+    }
+    this.timer = this.timer.bind(this)
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(this.timer, 1000)
+    // store intervalId in the state so it can be accessed later:
+    this.setState({intervalId})
+  }
+
+  componentWillUnmount() {
+    // use intervalId from the state to clear the interval
+    clearInterval(this.state.intervalId)
+  }
+
+  timer() {
+    // setState method is used to update the state
+    const newCount = this.state.currentCount - 1
+    if(newCount >= 0) {
+      this.setState({ currentCount: newCount })
+    } else {
+      clearInterval(this.state.intervalId)
     }
   }
 
   render() {
     // Return JSX via render()
-    const { minutes, seconds } = this.state
     return (
       <div>
-        <h1>{minutes} : {seconds}</h1>
+        <h1>{this.state.currentCount}</h1>
       </div>
     );
   }
