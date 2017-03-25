a3453012f2bcf47150324b296391b50e6d0954c7
Gan Jun Kai <kuhn96@gmail.com>
2017-03-25 16:23:39 +0800

    Modify the timer

--- app/components/Timer/Timer.js
+++ app/components/Timer/Timer.js
@@ -4,10 +4,12 @@ class Timer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentCount: 10,
+      currentMinuteCount: 10,
+      currentSecondCount: 60,
       intervalId: ''
     }
     this.timer = this.timer.bind(this)
+    this.nextMinute = this.nextMinute.bind(this)
   }
 
   componentDidMount() {
@@ -23,19 +25,36 @@ class Timer extends React.Component {
 
   timer() {
     // setState method is used to update the state
-    const newCount = this.state.currentCount - 1
-    if(newCount >= 0) {
-      this.setState({ currentCount: newCount })
+    let newSecondCount = this.state.currentSecondCount - 1
+    const newMinuteCount = this.state.currentMinuteCount - 1
+    if(newSecondCount >= 0) {
+      if(this.nextMinute(newSecondCount)) {
+        newSecondCount = 60
+        this.setState(
+          {
+            currentSecondCount: newSecondCount,
+            currentMinuteCount: newMinuteCount
+          }
+        )
+      } else {
+        this.setState({ currentSecondCount: newSecondCount })
+      }
     } else {
       clearInterval(this.state.intervalId)
     }
   }
 
+  nextMinute(currentSecond) {
+    return currentSecond === 0
+  }
+
   render() {
     // Return JSX via render()
+    const currentCount = `${this.state.currentMinuteCount} : ${this.state.currentSecondCount}`
+
     return (
       <div>
-        <h1>{this.state.currentCount}</h1>
+        <h1>{currentCount}</h1>
       </div>
     );
   }
