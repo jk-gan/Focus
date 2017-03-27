25416ed37c3d98525369463d756408780fda59f4
Gan Jun Kai <kuhn96@gmail.com>
2017-03-27 13:59:10 +0800

    Add Roboto font and add loader to load the font

--- config/loaders/fonts.js
+++ config/loaders/fonts.js
@@ -1,10 +1,10 @@
 module.exports = {
-  test: /\.(jpeg|jpg|png|gif|svg|eot|ttf|woff|woff2)$/i,
+  test: /\.(eot|ttf|woff|woff2)$/i,
   use: [{
     loader: 'url-loader',
     options: {
       limit: 10000,
-      name: 'images/[name]-[hash:8].[ext]'
+      name: 'fonts/[name]-[hash:8].[ext]'
     }
   }]
 }
